a961355675a423caf85bfdb93b6200b572975c2f
lenxia <15626508491@163.com>
2016-05-06 09:25:11 +0800

    first commit

---
+++ src/main/java/org/guohd/demo/App.java
@@ -0,0 +1,13 @@
+package org.guohd.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
